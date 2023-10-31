6a4a2a2bf05f8d9e2de5215f5a06bda4368e3e35
Jimmy Hidayat <jimmy.hidayat1019@gmail.com>
2023-10-31 17:27:37 +0700

    Video 3 : Struktur Folder, Routes & View

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => 'Jimmy Hidayat',
+        "email" => 'jimmy.hidayat@gmail.com',
+        "image" => 'Wall E.jpg'
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
