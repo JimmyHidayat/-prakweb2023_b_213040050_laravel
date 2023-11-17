25df36d74910272c1f3504fd67542b3223f476d2
Jimmy Hidayat <jimmy.hidayat1019@gmail.com>
2023-11-17 20:42:39 +0700

    Video 14 : View Login & Registration

--- coba-laravel/resources/views/partials/navbar.blade.php
+++ coba-laravel/resources/views/partials/navbar.blade.php
@@ -8,16 +8,23 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link {{($active === "Home") ? 'active' : ""}}" href="/">Home</a>
+                    <a class="nav-link {{ $active === 'Home' ? 'active' : '' }}" href="/">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{($active === "About") ? 'active' : ""}}" href="/about">About</a>
+                    <a class="nav-link {{ $active === 'About' ? 'active' : '' }}" href="/about">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{($active === "Posts") ? 'active' : ""}}" href="/blog">Blog</a>
+                    <a class="nav-link {{ $active === 'Posts' ? 'active' : '' }}" href="/blog">Blog</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{($active === "Categories") ? 'active' : ""}}" href="/categories">Categories</a>
+                    <a class="nav-link {{ $active === 'Categories' ? 'active' : '' }}"
+                        href="/categories">Categories</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a href="/login" class="nav-link {{ $active === 'login' ? 'active' : '' }}"><i class="bi bi-box-arrow-in-right"></i>
+                        Login</a>
                 </li>
             </ul>
         </div>
